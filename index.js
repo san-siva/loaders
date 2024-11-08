ad3ca880b3dd5c3e4f09e8abc148357418ab665e
Santhosh Siva <santhosh@Santhoshs-MacBook-Pro.local>
2024-11-08 10:20:26 +0530

    fix: added index.js file

---
+++ index.js
@@ -0,0 +1,10 @@
+// Tasks
+// 1. Create a text that says "Current count: 0"
+// 1. Create a primary_button with the text "Count up to 10"
+// 2. Create a secondary_button that says "Reset counter"
+// 2. Once the primary_button is clicked, we should show a loading spinner inside that primary_button
+// 3. Every second, the count should increase by 1, the text should update to reflect the current count ("Current count: 1", "Current count: 2", etc.)
+// 4. Once the count reaches 10, the loading spinner should disappear and the primary_button should be disabled
+// 5. If the secondary_button is clicked, the count should go back to 0 and the primary_button should be enabled again
+//
+// https://excalidraw.com/#room=7834ac5308d4ba695fe3,9FAj3m19ZS9z5z_otuEM9g
