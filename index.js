5e107c38c7768cc3426c6394b70560cde11511d7
Santhosh Siva <santhosh@Santhoshs-MacBook-Pro.local>
2024-11-10 12:51:00 +0530

    fix: Akshara learn stuff

--- index.js
+++ index.js
@@ -8,3 +8,74 @@
 // 5. If the secondary_button is clicked, the count should go back to 0 and the primary_button should be enabled again
 //
 // https://excalidraw.com/#room=7834ac5308d4ba695fe3,9FAj3m19ZS9z5z_otuEM9g
+//
+
+const button = document.querySelector('.button');
+const buttonText = document.getElementById('button-text');
+const countText = document.getElementById('count');
+
+let count = 0;
+
+const setCountTextToCount = () => {
+	countText.innerText = `Current count: ${count}`;
+};
+
+const setButtonState = (removeIsLoading = true) => {
+	if (removeIsLoading) {
+		button.classList.remove('button--loading');
+		button.classList.add('button--reset');
+		return;
+	}
+	button.classList.remove('button--reset');
+};
+
+const setInnerTextForButton = text => {
+	buttonText.innerText = text;
+};
+
+button.addEventListener('click', () => {
+	if (count === 5) {
+		count = 0;
+		setCountTextToCount();
+		setButtonState(false);
+		setInnerTextForButton('Start Counting');
+		return;
+	}
+	const interval = setInterval(() => {
+		count += 1;
+		setCountTextToCount();
+		if (count === 5) {
+			setButtonState();
+			setInnerTextForButton('Reset Counter');
+			clearInterval(interval);
+		}
+	}, 500);
+	// const classList = button.classList;
+	// const isButtonLoading = classList.contains('button--loading');
+	//
+	// if (isButtonLoading) {
+	// 	button.classList.remove('button--loading');
+	// 	return;
+	// }
+
+	button.classList.toggle('button--loading');
+});
+//
+// let timer1Value = 0;
+// let timer2Value = 0;
+//
+// const timerOne = setInterval(() => {
+// 	timer1Value += 1;
+// 	console.log('Iam timerOne', timer1Value);
+// 	// if (timer1Value === 5) {
+// 	// 	clearInterval(timerOne);
+// 	// }
+// }, 1000);
+//
+// const timerTwo = setInterval(() => {
+// 	timer2Value += 1;
+// 	console.log('Iam timerTwo', timer2Value);
+// 	if (timer2Value === 5) {
+// 		clearInterval(timerOne);
+// 	}
+// }, 1000);
